f473c6ea7f7e4a22d372fc8dd577ff093bdbfe80
trmtanjil <trmtanjil02@gmail.com>
2026-01-27 17:00:00 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+ import app from "./app";
+import { prisma } from "./lib/prisma";
+
+const PORT = process.env.PORT || 5000;
+
+async function main() {
+    try{
+        await prisma.$connect();
+ 
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    }catch(error){
+         await prisma.$disconnect();
+        process.exit(1);
+    }
+}
+main(); 
