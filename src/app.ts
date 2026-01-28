4fcaa648227a5e97e840be5729df305e408e1443
trmtanjil <trmtanjil02@gmail.com>
2026-01-28 22:00:34 +0600

    user register succesfully

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,13 @@
 import express,{Application} from "express"
 import { categoryRouter } from "./modules/category/category.route";
 import { medicineRouter } from "./modules/medicine/medicine.route";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 app.use(express.json());
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
 
 app.use("/category",categoryRouter)
 
