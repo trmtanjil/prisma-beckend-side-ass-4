5a928e7fb1200ec03cf5a0e9e8d383c6bf9bf0ae
trmtanjil <trmtanjil02@gmail.com>
2026-02-01 19:08:04 +0600

    port change

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
  import app from "./app";
 import { prisma } from "./lib/prisma";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 async function main() {
     try{
